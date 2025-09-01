c60ab2790098cd0ce073e061197062ec3c11005c
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2025-09-01 14:02:50 +0200

    Fix saveData function call to saveLocalData for correct localStorage functionality

--- assets/js/ModelLayer/model.js
+++ assets/js/ModelLayer/model.js
@@ -1,8 +1,12 @@
+// Gemmer alle lister til localStorage
+export function saveLocalData(appName = 'ToDoApp') {
+  localStorage.setItem(appName, JSON.stringify(userData));
+}
 export let userData = {
   lists: [],
   theme: 'light',
 };
 export function addFirstList() {
   userData.lists.push({ name: 'Min første liste', items: [] });
-  saveData();
+  saveLocalData();
 }
