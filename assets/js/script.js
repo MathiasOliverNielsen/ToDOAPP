870f15955288731c13fc058193bd43852075859e
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2025-09-01 08:41:52 +0200

    Jeg tror dette er rigtigt, sender til review

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-console.log('Weee! I am a JavaScript file!');
+// Funktion der kaldes ved opstart af Appen
+function userStartApp() {
+  const appName = 'ToDoApp';
+  // Hent data fra localStorage
+  const savedData = localStorage.getItem(appName);
+  let userData = {
+    lists: [],
+    //standard theme
+    theme: 'light',
+  };
+  if (savedData) {
+    userData = JSON.parse(savedData);
+  }
+  // Initialiser brugerens lister og tema
+  initUserLists(userData.lists);
+  setUserTheme(userData.theme);
+}
+// Kald userStartApp når siden er klar
+document.addEventListener('DOMContentLoaded', userStartApp);
